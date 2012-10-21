2868b243a095668139b83838395e7514319d8153
Anthony Lai <anthonyjlai@gmail.com>
2012-10-21 16:47:24 -0700

    Initialize vagrant_main cookbook

---
+++ cookbooks/vagrant_main/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "ANTHONY LAI"
+maintainer_email "anthonyjlai@gmail.com"
+license          "All rights reserved"
+description      "Installs/Configures vagrant_main"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
---
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: vagrant_main
+# Recipe:: default
+#
+# Copyright 2012, ANTHONY LAI
+#
+# All rights reserved - Do Not Redistribute
+#
